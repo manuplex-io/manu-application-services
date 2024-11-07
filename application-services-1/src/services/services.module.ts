91b1ded2593f93b350672cde8584eeb44eb2a35b
Abhishek <abhishek@manuplex.io>
2024-11-07 15:43:21 +0530

    changes

--- application-services-1/src/services/services.module.ts
+++ application-services-1/src/services/services.module.ts
@@ -1,11 +1,11 @@
-import { forwardRef, Module } from "@nestjs/common";
-import { OrderFormService } from "./orderform.service";
-import { KafkaOb1Module } from "src/kafka-ob1/kafka-ob1.module";
-
+import { forwardRef, Module } from '@nestjs/common';
+import { OrderFormService } from './orderform.service';
+import { KafkaOb1Module } from 'src/kafka-ob1/kafka-ob1.module';
+import { FindSupplierService } from './find-supplier.service';
 
 @Module({
-    imports:[forwardRef(() => KafkaOb1Module)],
-    providers:[OrderFormService],
-    exports:[OrderFormService]
+  imports: [forwardRef(() => KafkaOb1Module)],
+  providers: [OrderFormService, FindSupplierService],
+  exports: [OrderFormService, FindSupplierService],
 })
 export class ServicesModule {}
