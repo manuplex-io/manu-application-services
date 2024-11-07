9f11e30aa4d0e955dadaa12aa5a647be14b1795c
aadish33 <aadish@manuplex.io>
2024-11-07 14:32:37 +0530

    updated services.module.ts

--- application-services-1/src/services/services.module.ts
+++ application-services-1/src/services/services.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { OrderFormService } from "./orderform.service";
+import { KafkaOb1Module } from "src/kafka-ob1/kafka-ob1.module";
 
 
 @Module({
-    imports:[],
+    imports:[KafkaOb1Module],
     providers:[OrderFormService],
     exports:[OrderFormService]
 })
