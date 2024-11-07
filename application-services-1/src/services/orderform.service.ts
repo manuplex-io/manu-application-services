9aabc29b69eb69124052e99008e93275981a930a
aadish33 <aadish@manuplex.io>
2024-11-07 16:59:44 +0530

    changes for response format

--- application-services-1/src/services/orderform.service.ts
+++ application-services-1/src/services/orderform.service.ts
@@ -25,6 +25,188 @@ export class OrderFormService implements OnModuleInit {
   async getOrderForm(userInput: string, context: KafkaContext) {
     const messageHeaders = context.getMessage().headers as unknown as OB1MessageHeader
     const messageKey = context.getMessage().key?.toString();
+    const responseFormat = {
+        "type":"json_schema",
+        "json_schema":{
+            "name": "OrderForm",
+            "schema": {
+              "type": "object",
+              "properties": {
+                "order_summary": {
+                  "type": "string",
+                  "description": "Summary message regarding the order"
+                },
+                "material_type": {
+                  "type": "array",
+                  "description": "List of material types.",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "label": {
+                        "type": "string",
+                        "description": "The label of the material type"
+                      }
+                    },
+                    "required": [
+                      "label"
+                    ],
+                    "additionalProperties": false
+                  }
+                },
+                "manufacturing_process": {
+                  "type": "array",
+                  "description": "List of manufacturing processes.",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "label": {
+                        "type": "string",
+                        "description": "The label of the manufacturing process"
+                      }
+                    },
+                    "required": [
+                      "label"
+                    ],
+                    "additionalProperties": false
+                  }
+                },
+                "secondary_operations": {
+                  "type": "array",
+                  "description": "List of secondary operations.",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "label": {
+                        "type": "string",
+                        "description": "The label of the secondary operation"
+                      }
+                    },
+                    "required": [
+                      "label"
+                    ],
+                    "additionalProperties": false
+                  }
+                },
+                "finishing": {
+                  "type": "array",
+                  "description": "List of finishing types.",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "label": {
+                        "type": "string",
+                        "description": "The label of the finishing type"
+                      }
+                    },
+                    "required": [
+                      "label"
+                    ],
+                    "additionalProperties": false
+                  }
+                },
+                "product_certifications": {
+                  "type": "array",
+                  "description": "List of product certifications.",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "label": {
+                        "type": "string",
+                        "description": "The label of the product certification"
+                      }
+                    },
+                    "required": [
+                      "label"
+                    ],
+                    "additionalProperties": false
+                  }
+                },
+                "certifications": {
+                  "type": "array",
+                  "description": "List of certifications.",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "label": {
+                        "type": "string",
+                        "description": "The label of the certification"
+                      }
+                    },
+                    "required": [
+                      "label"
+                    ],
+                    "additionalProperties": false
+                  }
+                },
+                "facilities_infrastructure": {
+                  "type": "array",
+                  "description": "List of facilities or infrastructure.",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "label": {
+                        "type": "string",
+                        "description": "The label of the facility or infrastructure"
+                      }
+                    },
+                    "required": [
+                      "label"
+                    ],
+                    "additionalProperties": false
+                  }
+                },
+                "inspection_techniques": {
+                  "type": "array",
+                  "description": "List of inspection techniques.",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "label": {
+                        "type": "string",
+                        "description": "The label of the inspection technique"
+                      }
+                    },
+                    "required": [
+                      "label"
+                    ],
+                    "additionalProperties": false
+                  }
+                },
+                "region": {
+                  "type": "array",
+                  "description": "List of regions.",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "label": {
+                        "type": "string",
+                        "description": "The label of the region"
+                      }
+                    },
+                    "required": [
+                      "label"
+                    ],
+                    "additionalProperties": false
+                  }
+                }
+              },
+              "required": [
+                "order_summary",
+                "material_type",
+                "manufacturing_process",
+                "secondary_operations",
+                "finishing",
+                "product_certifications",
+                "certifications",
+                "facilities_infrastructure",
+                "inspection_techniques",
+                "region"
+              ],
+              "additionalProperties": false
+            },
+            "strict": true
+          }
+    }
     const messageInput = {
       messageContent: {
         functionInput: {
@@ -44,6 +226,7 @@ export class OrderFormService implements OnModuleInit {
                 - region: Extract ONLY if location is explicitly specified
             `,
           userPrompt: userInput,
+          responseFormat:responseFormat,
           config: {
             provider: 'openai',
             model: 'gpt-4o-mini',
