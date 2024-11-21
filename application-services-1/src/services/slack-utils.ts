4805d953caf7b904112b828871596bd7300cb5d5
aadish33 <aadish@manuplex.io>
2024-11-21 19:16:03 +0530

    changes

--- application-services-1/src/services/slack-utils.ts
+++ application-services-1/src/services/slack-utils.ts
@@ -5,13 +5,14 @@ export function createProjectBlocks(projects: any[]): any[] {
     projects.forEach((project) => {
         blocks.push({
           type: 'rich_text',
+          style: "bullet",
           elements: [
             {
               type: 'rich_text_section',
               elements: [
                 {
                   type: 'text',
-                  text: `*${project.projectName}*`
+                  text: `${project.projectName}`
                 }
               ]
             }
